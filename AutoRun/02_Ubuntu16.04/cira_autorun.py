7252ff0cc3393c5d9c448dafbc8d87b31dd5dae0
Jedtanut <jedtanut@outlook.com>
2022-02-28 16:40:31 +0700

    update

--- AutoRun/02_Ubuntu16.04/cira_autorun.py
+++ AutoRun/02_Ubuntu16.04/cira_autorun.py
@@ -28,11 +28,11 @@ time.sleep(3)
 
 # 3 run rtsp
 Popen(["gnome-terminal", '-x' , 'bash', '-c' ,'source /opt/ros/kinetic/setup.bash && source ~/catkin_ws/devel/setup.bash --extend && roslaunch rtsp_cam rtsp_cam.launch rtsp_url1:=%s rtsp_url2:=%s  ;$SHELL'%(rtsp_url1,rtsp_url2)])
-time.sleep(3)
+
 
 # 5 schedule reboot
 #Popen(["gnome-terminal", '-x' , 'bash', '-c' ,' python ~/cira_startup/schedule_reboot.py'])
-#time.sleep(2)
+time.sleep(2)
 
 import subprocess
 import shlex
@@ -46,7 +46,7 @@ def run_command(command):
             print output.strip()
     rc = process.poll()
     return rc
-
+time.sleep(3)
 # 6 run cira core
 Popen(["gnome-terminal", '-x' , 'bash', '-c' ,'LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/lib && source /opt/ros/kinetic/setup.bash && source ~/.cira_core_install/cira_libs_ws/install/setup.bash --extend && source ~/robot_ws/devel/setup.bash --extend && export LD_LIBRARY_PATH=$(find /usr/lib -iname nvidia-* -type d 2>&1 | sed "{:q;N;s/\\n/:/g;t q}"):${LD_LIBRARY_PATH} && export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/opt/qt511/lib && while true; do  rosrun cira_core cira_core_run _selected:=true _file:=%s _hide_toolbar:=true _fullscreen:=%s; sleep 2s; done ;$SHELL'%(cira_core_path,full_screen)])
 time.sleep(2)
