2c546d3d1fcf63684f3ee4f3dda45e893809da01
JedtanutSem <94428679+JedtanutSem@users.noreply.github.com>
2022-02-28 13:33:23 +0700

    [Add] Autorun

---
+++ AutoRun/02_Ubuntu16.04/Readme.rd
@@ -0,0 +1,8 @@
+Goto startup application
+
+
+: add cmd
+gnome-terminal -e 'python '/home/tss/robots_ws/startup/cira_autorun.py''
+
+//camera gstremer
+rtsp://admin:aaaa1234@192.168.1.64:554/Streaming/Channels/101
---
+++ AutoRun/03_Jetson/jetson_cira_autorun.py
@@ -0,0 +1,53 @@
+import os
+from urllib import unquote
+from subprocess import Popen
+import time
+
+
+print('###########  CiRA Autorun ############')
+import getpass
+username = getpass.getuser()
+print('user : ', username)
+
+
+time.sleep(3)
+
+
+# 1 run ros core
+Popen(["gnome-terminal", '-x' , 'bash', '-c' ,'LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/lib && source /opt/ros/melodic/setup.bash && roscore'])
+time.sleep(3)
+
+# 2 run cam_0
+Popen(["gnome-terminal", '-x' , 'bash', '-c' ,'source /opt/ros/melodic/setup.bash && source ~/.cira_core_install/cira_libs_ws/install/setup.bash --extend && roslaunch usb_cam usb_cam-test.launch cam_ns:="camera" cam_dev:="/dev/video0'])
+time.sleep(2)
+
+# 2 run cam_0
+Popen(["gnome-terminal", '-x' , 'bash', '-c' ,'source /opt/ros/melodic/setup.bash && source ~/.cira_core_install/cira_libs_ws/install/setup.bash --extend && roslaunch usb_cam usb_cam-test.launch cam_ns:="camera" cam_dev:="/dev/video1'])
+time.sleep(2)
+
+# 2 run cam_0
+Popen(["gnome-terminal", '-x' , 'bash', '-c' ,'source /opt/ros/melodic/setup.bash && source ~/.cira_core_install/cira_libs_ws/install/setup.bash --extend && roslaunch usb_cam usb_cam-test.launch cam_ns:="camera" cam_dev:="/dev/video2'])
+time.sleep(2)
+
+
+# 5 schedule reboot
+#Popen(["gnome-terminal", '-x' , 'bash', '-c' ,' python ~/cira_startup/schedule_reboot.py'])
+#time.sleep(2)
+
+import subprocess
+import shlex
+def run_command(command):
+    process = subprocess.Popen(command, stdout=subprocess.PIPE)
+    while True:
+        output = process.stdout.readline()
+        if output == '' and process.poll() is not None:
+            break
+        if output:
+            print output.strip()
+    rc = process.poll()
+    return rc
+
+# 6 run cira core
+Popen(["gnome-terminal", '-x' , 'bash', '-c' ,'LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/lib && source /opt/ros/melodic/setup.bash && source ~/.cira_core_install/cira_libs_ws/install/setup.bash --extend && export LD_LIBRARY_PATH=$(find /usr/lib -iname nvidia-* -type d 2>&1 | sed "{:q;N;s/\\n/:/g;t q}"):${LD_LIBRARY_PATH} && export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/opt/qt511/lib && rosrun cira_core cira_core_run _file:=\'/home/cira/Desktop/Master_Project/Sealer_Inspection.npj\' ;$SHELL'])
+time.sleep(2)
+
