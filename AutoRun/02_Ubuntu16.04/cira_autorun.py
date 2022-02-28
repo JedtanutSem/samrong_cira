b67437f9d10a9e45b2c4b250c238fbd006821784
Jedtanut <jedtanut@outlook.com>
2022-02-28 16:49:18 +0700

    up

--- AutoRun/02_Ubuntu16.04/cira_autorun.py
+++ AutoRun/02_Ubuntu16.04/cira_autorun.py
@@ -48,6 +48,6 @@ def run_command(command):
     return rc
 time.sleep(3)
 # 6 run cira core
-Popen(["gnome-terminal", '-x' , 'bash', '-c' ,'LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/lib && source /opt/ros/kinetic/setup.bash && source ~/.cira_core_install/cira_libs_ws/install/setup.bash --extend && source ~/robot_ws/devel/setup.bash --extend && export LD_LIBRARY_PATH=$(find /usr/lib -iname nvidia-* -type d 2>&1 | sed "{:q;N;s/\\n/:/g;t q}"):${LD_LIBRARY_PATH} && export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/opt/qt511/lib && while true; do  rosrun cira_core cira_core_run _selected:=true _file:=%s _hide_toolbar:=true _fullscreen:=%s; sleep 2s; done ;$SHELL'%(cira_core_path,full_screen)])
+Popen(["gnome-terminal", '-x' , 'bash', '-c' ,'LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/lib && source /opt/ros/kinetic/setup.bash && source ~/.cira_core_install/cira_libs_ws/install/setup.bash --extend && source ~/robot_ws/devel/setup.bash --extend && export LD_LIBRARY_PATH=$(find /usr/lib -iname nvidia-* -type d 2>&1 | sed "{:q;N;s/\\n/:/g;t q}"):${LD_LIBRARY_PATH} && export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/opt/qt511/lib  rosrun cira_core cira_core_run _selected:=true _file:=%s _hide_toolbar:=true _fullscreen:=%s; sleep 2s; done ;$SHELL'%(cira_core_path,full_screen)])
 time.sleep(2)
 print('####### END #######')
